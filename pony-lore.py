1e461ed19e300fc3ede91abd583c526d00faf0d9
JPisaBrony <jdpohl789@gmail.com>
2015-11-02 14:15:43 -0600

    + source code

---
+++ pony-lore.py
@@ -0,0 +1,251 @@
+import pygame, sys
+from pygame.locals import *
+
+WINDOW_WIDTH = 768
+WINDOW_HEIGHT = 1024
+show_map = 0
+
+class Tile:
+    def __init__(self, x, y, w, h, color, edge_color):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.n_edge = 0
+        self.e_edge = 0
+        self.s_edge = 0
+        self.w_edge = 0
+        self.edge_color = edge_color
+    
+    def set_edge(self, dir, val):
+        if dir == 0:
+            self.n_edge = val
+        if dir == 1:
+            self.e_edge = val
+        if dir == 2:
+            self.s_edge = val
+        if dir == 3:
+            self.w_edge = val
+
+class Map:
+    def __init__(self, map_size):
+        squares = [[0 for x in range(map_size)] for x in range(map_size)]
+        size = 50
+        color = (255, 255, 255)
+        edge_color = (200, 200, 200)
+        
+        for i in range(10):
+            self.add_to_map(squares, i, 0, size, color, edge_color)
+        
+        for i in range(10):
+            self.add_to_map(squares, 9, i, size, color, edge_color)
+ 
+        for i in range(10):
+            self.add_to_map(squares, i, 4, size, color, edge_color)
+            
+        for i in range(10):
+            self.add_to_map(squares, 4, i, size, color, edge_color)
+        
+        self.tiles = squares
+        self.add_edges()
+    
+    def add_edges(self):
+        for i in range(len(self.tiles)):
+            for j in range(len(self.tiles[0])):
+                if self.tiles[i][j] != 0:
+                    if i+1 <= 9:
+                        if self.tiles[i+1][j] == 0:
+                            self.tiles[i][j].e_edge = 1
+                    else:
+                        self.tiles[i][j].e_edge = 1
+                    if i-1 >= 0:
+                        if self.tiles[i-1][j] == 0:
+                            self.tiles[i][j].w_edge = 1
+                    else:
+                        self.tiles[i][j].w_edge = 1
+                    if j+1 <= 9:
+                        if self.tiles[i][j+1] == 0:
+                            self.tiles[i][j].s_edge = 1
+                    else:
+                        self.tiles[i][j].s_edge = 1
+                    if j-1 >= 0:
+                        if self.tiles[i][j-1] == 0:
+                            self.tiles[i][j].n_edge = 1
+                    else:
+                        self.tiles[i][j].n_edge = 1
+
+    def add_to_map(self, squares, x, y, size, color, edge_color):
+        squares[x][y] = Tile(size * x, size * y, size, size, color, edge_color)
+
+    def render_map(self, window):
+        for i in self.tiles:
+            for j in i:
+                if j != 0:
+                    pygame.draw.rect(window, j.color, (j.x, j.y, j.w, j.h))
+                    if j.n_edge:
+                        pygame.draw.rect(window, j.edge_color, (j.x, j.y, j.w, 2))
+                    if j.e_edge:
+                        pygame.draw.rect(window, j.edge_color, (j.x + j.w - 2, j.y, 2, j.h))
+                    if j.s_edge:
+                        pygame.draw.rect(window, j.edge_color, (j.x, j.y + j.h - 2, j.w, 2))
+                    if j.w_edge:
+                        pygame.draw.rect(window, j.edge_color, (j.x, j.y, 2, j.h))
+    
+    def render_3d_map(self, window, player):       
+        player_tile = 0
+        if player.dir == 0 and player.tile_y >= 0 and player.tile_y <= 9:
+            player_tile = self.tiles[player.tile_x][player.tile_y-1]
+        elif player.dir == 1 and player.tile_x >= 0 and player.tile_x <= 9:
+            player_tile = self.tiles[player.tile_x+1][player.tile_y]
+        elif player.dir == 2 and player.tile_y >= 0 and player.tile_y <= 9:
+            player_tile = self.tiles[player.tile_x][player.tile_y+1]
+        elif player.dir == 3 and player.tile_x >= 0 and player.tile_x <= 9:
+            player_tile = self.tiles[player.tile_x-1][player.tile_y]
+        
+        if player_tile == 0:
+            return
+        
+        if player.dir == 0:
+            if player_tile.w_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            if player_tile.e_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            if player_tile.n_edge == 1:
+                pygame.draw.rect(window, (200, 200, 200), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+            else:
+                pygame.draw.rect(window, (0, 0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+        if player.dir == 1:
+            if player_tile.n_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            if player_tile.s_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            if player_tile.e_edge == 1:
+                pygame.draw.rect(window, (200, 200, 200), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+            else:
+                pygame.draw.rect(window, (0, 0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+        if player.dir == 2:
+            if player_tile.e_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            if player_tile.w_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            if player_tile.s_edge == 1:
+                pygame.draw.rect(window, (200, 200, 200), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+            else:
+                pygame.draw.rect(window, (0, 0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+        if player.dir == 3:
+            if player_tile.s_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4), (WINDOW_HEIGHT / 4, (WINDOW_WIDTH / 4) * 3), (0, WINDOW_WIDTH)))
+            if player_tile.n_edge == 1:
+                pygame.draw.polygon(window, (255, 255, 255), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            else:
+                pygame.draw.polygon(window, (0, 0, 0), ((WINDOW_HEIGHT, 0), ((WINDOW_HEIGHT / 4) * 3, WINDOW_WIDTH / 4), ((WINDOW_HEIGHT / 4) * 3, (WINDOW_WIDTH / 4) * 3), (WINDOW_HEIGHT, WINDOW_WIDTH)))
+            if player_tile.w_edge == 1:
+                pygame.draw.rect(window, (200, 200, 200), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+            else:
+                pygame.draw.rect(window, (0, 0, 0), (WINDOW_HEIGHT / 4, WINDOW_WIDTH / 4, WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+
+class Player:
+    def __init__(self, tile_size):
+        self.x = tile_size / 4
+        self.y = tile_size / 4
+        self.tile_x = 0
+        self.tile_y = 0
+        self.color = (100, 100, 100)
+        self.w = tile_size / 2
+        self.h = tile_size / 2
+        self.dir = 1
+        self.drawDir = 0
+        self.tile_size = tile_size
+    
+    def render_player(self, window):
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.w, self.h))
+    
+    def move_forward(self):
+        if self.dir == 0:
+            self.y -= self.tile_size
+            self.tile_y -= 1
+        elif self.dir == 1:
+            self.x += self.tile_size
+            self.tile_x += 1
+        elif self.dir == 2:
+            self.y += self.tile_size
+            self.tile_y += 1
+        elif self.dir == 3:
+            self.x -= self.tile_size
+            self.tile_x -= 1
+    
+    def rotate_left(self):
+        if self.dir > 0:
+            self.dir -= 1
+        else:
+            self.dir = 3
+        if self.drawDir > 0:
+            self.drawDir -= 1
+        else:
+            self.drawDir = 3
+    
+    def rotate_right(self):
+        if self.dir < 3:
+            self.dir += 1
+        else:
+            self.dir = 0
+        if self.drawDir < 3:
+            self.drawDir += 1
+        else:
+            self.drawDir = 0
+
+def handle_events(player):
+    global show_map
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                player.move_forward()
+            elif event.key == K_LEFT:
+                player.rotate_left()
+            elif event.key == K_RIGHT:
+                player.rotate_right()
+            elif event.key == K_m:
+                if show_map == 0:
+                    show_map = 1
+                else:
+                    show_map = 0
+
+def draw(window, test_map, player):
+    global show_map
+    window.fill((0, 0, 0))
+    if show_map == 0:
+        test_map.render_3d_map(window, player)
+    else:
+        test_map.render_map(window)
+        player.render_player(window)
+    pygame.display.flip()
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
+    test_map = Map(10)
+    player = Player(50)
+    while True:
+        handle_events(player)
+        draw(window, test_map, player)
+
+if __name__ == "__main__":
+    main()
